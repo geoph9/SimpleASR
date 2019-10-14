a490ff28f6962c49607e18f92983850d32098177
George K <george.karakasides@gmail.com>
2019-10-14 10:58:37 +0300

    Update put_users_to_folders.py

--- put_users_to_folders.py
+++ put_users_to_folders.py
@@ -9,10 +9,10 @@ test_path = os.path.join(path_to_waves, "test")
 for filename in glob.glob(train_path + "/*.wav"):
     basename = os.path.basename(filename)
     speaker_id = basename.split("_")[1]
-    shutil.move(filename, os.path.join(train_path, speaker_id) + basename)
+    shutil.move(filename, os.path.join(train_path, speaker_id, basename))
 
 for filename in glob.glob(test_path + "/*.wav"):
     basename = os.path.basename(filename)
     speaker_id = basename.split("_")[1]
-    shutil.move(filename, os.path.join(test_path, speaker_id) + basename)
+    shutil.move(filename, os.path.join(test_path, speaker_id, basename))
 print("Success")
